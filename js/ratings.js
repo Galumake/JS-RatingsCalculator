dc9bf436dcbe4f2c6f1b32ae12cee31de7e79b74
Alfonso <alfonso.uma@gmail.com>
2022-12-06 16:38:15 -0600

    x

--- js/ratings.js
+++ js/ratings.js
@@ -28,4 +28,4 @@ document.addEventListener("change", () => {
   const ratings = collect_ratings();
   document.querySelector("#average").value = ratings.average.toFixed(2);
 });
-/** THIS IS MY COMMENT */
+/** THIS IS MY COMMENT new comment*/
